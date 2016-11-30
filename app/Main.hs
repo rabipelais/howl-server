9f7771f220458ea9d6f276d2de3fab3e8e3fa229
Sebastian Mendez <sebas.chinoir@gmail.com>
2016-11-30 23:05:59 +0100

    Get app credentials from config file (not in repo).

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import App
+import qualified Facebook as Fb
+import Data.Configurator as C
+
 --------------------------------------------------
 
+getCredentials :: FilePath -> IO (Maybe Fb.Credentials)
+getCredentials filePath = do
+  config <- load [Required filePath]
+  appName <- C.lookup config "credentials.appName"
+  appId <- C.lookup config "credentials.appId"
+  appSecret <- C.lookup config "credentials.appSecret"
+  return $ Fb.Credentials <$> appName <*> appId <*> appSecret
+
 main :: IO ()
-main = run ":memory:"
+main = do
+  mCreds <- getCredentials "app.cfg"
+  case mCreds of
+    Nothing -> print "ERROR reading config file"
+    Just creds -> run "sqlitetest.db" creds
