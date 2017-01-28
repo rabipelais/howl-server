3bf74c2832d0a6d2475d0601c9f16a016fe28b0c
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-01-28 17:01:48 +0100

    Change Main driver to Postgresql.

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import           Data.Configurator        as C
+import           Data.Configurator           as C
 import           Howl
-import qualified Howl.Facebook            as Fb
-import qualified Howl.Logger              as Logger
+import qualified Howl.Facebook               as Fb
+import qualified Howl.Logger                 as Logger
 
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
 
 import           Data.String.Conversions
 
+import           Data.ByteString.Char8       (pack)
 import           Database.Persist
+import           Database.Persist.Postgresql
 import           Database.Persist.Sql
-import           Database.Persist.Sqlite
 
-import           Network.HTTP.Conduit     (Manager, newManager,
-                                           tlsManagerSettings)
+import           Network.HTTP.Conduit        (Manager, newManager,
+                                              tlsManagerSettings)
 import           Network.Wai
-import           Network.Wai.Handler.Warp as Warp
+import           Network.Wai.Handler.Warp    as Warp
 
 --------------------------------------------------
 
@@ -32,23 +33,29 @@ getCredentials config = do
 main :: IO ()
 main = do
   let configPath = "app.cfg"
+  putStrLn "Reading config..."
   config <- load [Required configPath]
   mCreds <- getCredentials config
-  (Just (dbName :: String)) <- C.lookup config "database.dbName"
+  (Just (dbName :: String)) <- C.lookup config "database.name"
+  (Just (dbHost :: String)) <- C.lookup config "database.host"
+  (Just (dbUser :: String)) <- C.lookup config "database.user"
+  (Just (dbPassword :: String)) <- C.lookup config "database.password"
+  (Just (dbPort :: String)) <- C.lookup config "database.port"
   (Just poolSize) <- C.lookup config "database.poolSize"
   let logSettings = Logger.Settings
         { Logger.filePath = "webserver.log"
         , Logger.logLevel = LevelDebug
         , Logger.noConsoleLogging = False
         }
+      connString = pack $ "host=" <> dbHost <> " dbname=" <> dbName <> " user=" <> dbUser <> " password=" <> dbPassword <> " port=" <> dbPort
   putStrLn "Starting server"
   case mCreds of
     Nothing -> print "ERROR reading config file"
     Just creds -> Logger.withLogger logSettings $ \logFn -> do
       let port = 3000 :: Int
       liftIO $ putStrLn $ "Listening on port " ++ show port ++ " ..."
-      pool <- runStderrLoggingT $ createSqlitePool (cs dbName) poolSize
-      runSqlPool (runMigration migrateAll) pool
-      manager <- liftIO $ newManager tlsManagerSettings
-      let env = LogEnv logFn $ authHandlerEnv pool manager creds
-      liftIO $ Warp.run port $ app env
+      runStderrLoggingT $ withPostgresqlPool connString poolSize $ \pool -> do
+        runSqlPool (runMigration migrateAll) pool
+        manager <- liftIO $ newManager tlsManagerSettings
+        let env = LogEnv logFn $ authHandlerEnv pool manager creds
+        liftIO $ Warp.run port $ app env
