c525dd95a22fe31daac01c92a8418386399baed2
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-03-02 03:16:17 +0100

    Change log file name and redirect in docker to stdout.

--- app/Main.hs
+++ app/Main.hs
@@ -34,7 +34,7 @@ main = do
   putStrLn "Reading config..."
   (dbName, dbHost, dbUser, dbPassword, dbPort, poolSize, mqHost, mqVHost, mqUser, mqPassword, creds) <- getConfig
   let logSettings = Logger.Settings
-        { Logger.filePath = "webserver.log"
+        { Logger.filePath = "/var/log/webserver.log"
         , Logger.logLevel = LevelDebug
         , Logger.noConsoleLogging = False
         }
