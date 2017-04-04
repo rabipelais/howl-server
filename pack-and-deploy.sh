d78f4f3f8a78d91d31dfe9587707b1889bfb37bb
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-04-04 11:52:54 +0200

    Update deploy script to make it generic.

--- pack-and-deploy.sh
+++ pack-and-deploy.sh
@@ -3,11 +3,18 @@
 
 SHA1=$(git rev-parse --short HEAD)
 
-#Webserver
-docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-webserver -f Dockerfile.deploy .
+if [ $1 = "webserver" ]
+then
+	APPLICATION=Howl-Backend
+	ENVIRONMENT=Sample-env
+fi
+if [ $1 = "notifications" ]
+then
+	APPLICATION=Howl-Notifications
+	ENVIRONMENT=LowCost-env
+fi
 
-#Notifications
-docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-notifications -f Dockerfile.notifications.deploy .
+docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-$1 -f Dockerfile.$1.deploy .
 
 
 mkdir ~/.aws/
@@ -18,36 +25,18 @@ printf "region=eu-central-1\noutput=json" >> ~/.aws/config
 
 # Push image to ECR
 $(aws ecr get-login --region eu-central-1)
-docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:$SHA1-webserver
-
-docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:$SHA1-notifications
+docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:$SHA1-$1
 
 # Create new Elastic Beanstalk version
-#Webserver
 EB_BUCKET=howl-deploy-bucket
-DOCKERRUN_FILE=$SHA1-webserver-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1-webserver/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+DOCKERRUN_FILE=$SHA1-$1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1-$1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region eu-central-1
-aws elasticbeanstalk create-application-version --application-name Howl-Backend \
-    --version-label $SHA1-webserver --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
-    --region eu-central-1
-
-#Notifications
-EB_BUCKET=howl-deploy-bucket
-DOCKERRUN_FILE=$SHA1-notifications-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1-notifications/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region eu-central-1
-aws elasticbeanstalk create-application-version --application-name Howl-Notifications \
-    --version-label $SHA1-notifications --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
+aws elasticbeanstalk create-application-version --application-name $APPLICATION \
+    --version-label $SHA1-$1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
     --region eu-central-1
 
 # Update Elastic Beanstalk environment to new version
-#Webserver
-aws elasticbeanstalk update-environment --environment-name Sample-env \
-    --version-label $SHA1-webserver \
-    --region eu-central-1
-
-#Notifications
-aws elasticbeanstalk update-environment --environment-name LowCost-env \
-    --version-label $SHA1-notifications \
+aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT \
+    --version-label $SHA1-$1 \
     --region eu-central-1
