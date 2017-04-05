3912fe8b2a272e7a707d792e80a3cacc9e7959f1
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-04-05 17:22:56 +0200

    Modify Amazon apps and environments in deployment script.

--- pack-and-deploy.sh
+++ pack-and-deploy.sh
@@ -3,18 +3,10 @@
 
 SHA1=$(git rev-parse --short HEAD)
 
-if [ $1 = "webserver" ]
-then
-	APPLICATION=Howl-Backend
-	ENVIRONMENT=Sample-env
-fi
-if [ $1 = "notifications" ]
-then
-	APPLICATION=Howl-Notifications
-	ENVIRONMENT=LowCost-env
-fi
+APPLICATION=$1
+ENVIRON=$2
 
-docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-$1 -f Dockerfile.$1.deploy .
+docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-$APPLICATION-$ENVIRON -f Dockerfile.$APPLICATION.deploy .
 
 
 mkdir ~/.aws/
@@ -25,18 +17,18 @@ printf "region=eu-central-1\noutput=json" >> ~/.aws/config
 
 # Push image to ECR
 $(aws ecr get-login --region eu-central-1)
-docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:$SHA1-$1
+docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:$SHA1-$APPLICATION-$ENVIRON
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=howl-deploy-bucket
-DOCKERRUN_FILE=$SHA1-$1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1-$1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+DOCKERRUN_FILE=$SHA1-$APPLICATION-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1-$APPLICATION-$ENVIRON/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region eu-central-1
 aws elasticbeanstalk create-application-version --application-name $APPLICATION \
-    --version-label $SHA1-$1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
+    --version-label $SHA1-$APPLICATION-$ENVIRON --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
     --region eu-central-1
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT \
-    --version-label $SHA1-$1 \
+aws elasticbeanstalk update-environment --environment-name Howl-$APPLICATION-$ENVIRONMENT \
+    --version-label $SHA1-$APPLICATION-$ENVIRON \
     --region eu-central-1
