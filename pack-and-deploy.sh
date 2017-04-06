40017015a4eff6d63ff6fa3860db3b9c1492a268
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-04-06 19:11:16 +0200

    Modify deployment script to build zip with ebextensions.

--- pack-and-deploy.sh
+++ pack-and-deploy.sh
@@ -4,16 +4,21 @@
 SHA1=$(git rev-parse --short HEAD)
 
 APPLICATION=$1
+APPLICATION_NAME=$1
+if [ "$APPLICATION" == "Webserver" ]
+then
+	APPLICATION_NAME="Backend"
+fi
 ENVIRON=$2
 
-docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-$APPLICATION-$ENVIRON -f Dockerfile.$APPLICATION.deploy .
+docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$AWS_REPO_NAME:$SHA1-$APPLICATION-$ENVIRON -f Dockerfile.${APPLICATION,,}.deploy .
 
 
-mkdir ~/.aws/
-touch ~/.aws/credentials
-printf "aws_access_key_id = %s\naws_secret_access_key = %s\n" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
-touch ~/.aws/config
-printf "region=eu-central-1\noutput=json" >> ~/.aws/config
+#mkdir ~/.aws/
+#touch ~/.aws/credentials
+#printf "aws_access_key_id = %s\naws_secret_access_key = %s\n" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+#touch ~/.aws/config
+#printf "region=eu-central-1 \n output=json" >> ~/.aws/config
 
 # Push image to ECR
 $(aws ecr get-login --region eu-central-1)
@@ -23,12 +28,13 @@ docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:
 EB_BUCKET=howl-deploy-bucket
 DOCKERRUN_FILE=$SHA1-$APPLICATION-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1-$APPLICATION-$ENVIRON/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region eu-central-1
-aws elasticbeanstalk create-application-version --application-name $APPLICATION \
-    --version-label $SHA1-$APPLICATION-$ENVIRON --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
+zip -r $DOCKERRUN_FILE.zip .ebextensions $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$DOCKERRUN_FILE.zip --region eu-central-1
+aws elasticbeanstalk create-application-version --application-name Howl-$APPLICATION_NAME \
+    --version-label $SHA1-$APPLICATION-$ENVIRON --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE.zip \
     --region eu-central-1
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name Howl-$APPLICATION-$ENVIRONMENT \
+aws elasticbeanstalk update-environment --environment-name Howl-$APPLICATION-$ENVIRON \
     --version-label $SHA1-$APPLICATION-$ENVIRON \
     --region eu-central-1
