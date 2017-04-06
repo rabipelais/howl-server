cb296c150cfa5c3f5625431adf3966025e8b974c
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-04-06 19:36:39 +0200

    Change output Dockerrun file name.

--- pack-and-deploy.sh
+++ pack-and-deploy.sh
@@ -27,8 +27,8 @@ docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/howl-docker-repo:
 # Create new Elastic Beanstalk version
 EB_BUCKET=howl-deploy-bucket
 DOCKERRUN_FILE=$SHA1-$APPLICATION-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1-$APPLICATION-$ENVIRON/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-zip -r $DOCKERRUN_FILE.zip .ebextensions $DOCKERRUN_FILE
+sed "s/<TAG>/$SHA1-$APPLICATION-$ENVIRON/" < Dockerrun.aws.json.template > Dockerrun.aws.json
+zip -r $DOCKERRUN_FILE.zip .ebextensions Dockerrun.aws.json
 aws s3 cp $DOCKERRUN_FILE.zip s3://$EB_BUCKET/$DOCKERRUN_FILE.zip --region eu-central-1
 aws elasticbeanstalk create-application-version --application-name Howl-$APPLICATION_NAME \
     --version-label $SHA1-$APPLICATION-$ENVIRON --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE.zip \
